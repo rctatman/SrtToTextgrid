52a6eab685d7e0c4dab6de9ca26f38603b7ae806
Rachael Tatman <rctatman@users.noreply.github.com>
2016-02-26 15:04:19 -0800

    Working now!

--- SrtToTextgrid.py
+++ SrtToTextgrid.py
@@ -1,5 +1,5 @@
 # this Python script takes a .srt file (common sub title formant) and converts it to a Praat .textgrid file
-# it *may* work on other types of files, but please be cautious 
+# it's general enough that it *may* work on other types of files like .sub, .sbv, but please be cautious 
 #
 # written by Rachael Tatman, please rctatman@uw.edu with any problems
 
@@ -8,18 +8,23 @@ import datetime
 import re
 import numpy
 import string
+from string import digits
+
+# it would be nice to ask user for file name and name for converted file from command line
+inputFile = "ccSubs_com_keynote-katie-bell-how-python-works-as-a-teaching-language_en.srt" # put the name of the file you'd like converted here
+outputFile = "output_katie.textgrid" # put what you'd like your output file to be called here
 
 # open .srt file
-file = open('ccSubs_com_jessica-mckellar-keynote-pycon-2014_en.srt', 'rw')
-with open('ccSubs_com_jessica-mckellar-keynote-pycon-2014_en.srt', 'r') as f:
+file = open(inputFile, 'rw')
+with open(inputFile, 'r') as f:
 	read_data = f.read().replace('\n', ' ')
 	read_data += "0" #add a numeric character at the end for our regex to work
 
 # create (or overwrite) new file for our output
-file1 = open('subs.textgrid','w')
+file1 = open(outputFile,'w')
 
 # first, though, we need to find out how long our file is in miliseconds,  
-pattern = re.compile("[0-9]*\:[0-9]*\:[0-9]*") # look for time-like things (09:08:00)
+pattern = re.compile("[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]") # look for time-like things (09:08:00)
 listOfTimes = [] 
 
 # grab all the times in the file
@@ -39,16 +44,16 @@ numberOfIntervals = times.size/2
 maxTime = max(times)
 
 # now let's extract the text
-pattern2 = re.compile("\d([a-zA-Z\' ]+)\d") # this matches any alphabetic characters and white spaces between numeric characters 
+# will this split when someone says a number, too?
+pattern2 = re.compile("\:\d\d([a-zA-Z\d\'.,%\&;/\$# ]+)[\d\s]*") # this matches any alphabetic characters, white spaces and assorted puncutuation between numeric characters 
 text = [] 
 
 for match in re.findall(pattern2, read_data): # find all text between numeric characters
 	text.append(match)
 
+text = [x.rstrip('1234567890 ') for x in text] #get rid of extra numbers at end of line
 text = [x.strip(' ') for x in text] #strip extra white spaces
-text = [x for x in text if x] # remove empty items 
-print(numberOfIntervals)
-print(len(text))
+text = [x for x in text if x]  # remove empty items
 
 # write textgrid preamble
 file1.write("File type = \"ooTextFile\"" + '\n')
@@ -73,12 +78,7 @@ for i in range(1,numberOfIntervals + 1): # I know it's weird, but I was getting 
 	file1.write("  intervals [" + str(i) + "]:" + "\n")
 	file1.write("            xmin = " + str(times.item((i * 2) - 2)) + "\n") # get the ith element from the array
 	file1.write("            xmax = " + str(times.item((i * 2 ) - 1)) + "\n") # ith element + 1
-#	file1.write("            text = \"" + text[i] + "\"\n") 
-
-## OK WE"VE GOT DIFFERNT NUMVERS OF INTERVALS AND TEXT AND THAT WON"T WORK
-
-# where am I loosing text??
-
+	file1.write("            text = \"" + text[i - 1] + "\"\n") 
 
 # close our files and tidy up
 file.close()
